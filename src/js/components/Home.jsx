8f0b8e2199f30e32a98280aa40ed2af79c650439
mcortez2308 <bensirave@gmail.com>
2026-02-18 23:52:56 +0000

    mi proyecto de todo list useeffects

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { renderToNodeStream } from "react-dom/server";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [inputValue, setInputValue ] = useState("");
+	const [todos, setTodos] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	function handleChange({target}){
+		setInputValue(target.value)
+	}
+
+	function addTask({key}){
+		if (key == "Enter"){
+			if(inputValue.trim() == ""){
+				console.log("La tarea debe tenr valor")
+				return
+			}
+			setTodos((prev)=>prev.concat([inputValue]))
+			setInputValue("")
+		}
+	}
+
+	function deleteTask(index){
+		setTodos((prev)=>prev.filter((_, i)=>i != index))
+	}
+
+	return (
+		<div className="container">        
+			<h1 className="text-center mt-5">ToDo List</h1>
+			<ul>
+				<li>
+					<input type="text" 
+							placeholder="Agrega tu tarea"
+							value={inputValue}
+							onChange={handleChange}
+							onKeyDown={addTask}/>
+				</li>
+				{
+					todos.map((task, index)=>
+					<li key={index}>
+						{task}
+						<span
+							onClick={()=>deleteTask(index)}
+						>X</span>
+					</li> )				
+				}
+			</ul>
 		</div>
 	);
 };
