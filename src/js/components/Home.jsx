6f84ffc71fac20d1b8c8dd268ae68cf3cef5a6e4
mcortez2308 <bensirave@gmail.com>
2026-02-19 00:09:08 +0000

    Toques finales

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,49 +4,54 @@ import { renderToNodeStream } from "react-dom/server";
 
 
 const Home = () => {
-	const [inputValue, setInputValue ] = useState("");
+	const [inputValue, setInputValue] = useState("");
 	const [todos, setTodos] = useState([]);
 
-	function handleChange({target}){
+	function handleChange({ target }) {
 		setInputValue(target.value)
 	}
 
-	function addTask({key}){
-		if (key == "Enter"){
-			if(inputValue.trim() == ""){
+	function addTask({ key }) {
+		if (key == "Enter") {
+			if (inputValue.trim() == "") {
 				console.log("La tarea debe tener valor")
 				return
 			}
-			setTodos((prev)=>prev.concat([inputValue]))
+			setTodos((prev) => prev.concat([inputValue]))
 			setInputValue("")
 		}
 	}
 
-	function deleteTask(index){
-		setTodos((prev)=>prev.filter((_, i)=>i != index))
+	function deleteTask(index) {
+		setTodos((prev) => prev.filter((_, i) => i != index))
 	}
 
 	return (
-		<div className="container">        
-			<h1 className="text-center mt-5">ToDo List</h1>
-			<ul>
-				<li>
-					<input type="text" 
-							placeholder="Agrega tu tarea"
-							value={inputValue}
-							onChange={handleChange}
-							onKeyDown={addTask}/>
-				</li>
-				{
-					todos.map((task, index)=>
-					<li key={index}>
-						{task}
-						<span
-							onClick={()=>deleteTask(index)}
-						>X</span>
-					</li> )				
-				}
-			</ul>
+		<div className="container">
+			<div className="row justify-content-center">
+				<div className="col-12 col-md-7">
+					<h1 className="text-center mt-5">ToDo List</h1>
+					<input type="text"
+						placeholder="Agrega tu tarea"
+						value={inputValue}
+						onChange={handleChange}
+						onKeyDown={addTask}
+					/>
+					<ul>
+						{
+							todos.map((task, index) =>
+								<li key={index}>
+									{task}
+									<span
+										onClick={() => deleteTask(index)}
+									>X</span>
+								</li>)
+						}
+					</ul>
+					<p>{todos.length} task left</p>
+				</div>
+			</div>
+
 		</div>
 	);
 };
